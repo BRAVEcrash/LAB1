e5dbde9903b1b3544d38708a49d8e5c01778e949
BRAVE <102480883+BRAVEcrash@users.noreply.github.com>
2024-09-08 23:01:19 +0900

    lab1

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    void compute() {
+        System.out.println("이릅: 잘랄로브바이쉬");
+        System.out.println("소속: 소프트웨어학부");
+        System.out.println("학번: 20243107");
+    }
+}
